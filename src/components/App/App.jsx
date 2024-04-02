7a004f68489731eb883cadd5aca86cc44a67a546
didukhroma <didukhroma@gmail.com>
2024-04-02 17:20:12 +0300

    feature: change state holder

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,60 @@
-import Feedback from 'components/Feedback';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
+import FeedbackOptions from 'components/FeedbackOptions';
+import Section from 'components/Section';
+import Statistics from 'components/Statistics';
+
+import { StyledMainTitle } from './App.styled';
+
+export default class App extends Component {
+  static defaultProps = {
+    step: 1,
+    initialValue: 0,
+  };
+
+  state = {
+    good: this.props.initialValue,
+    neutral: this.props.initialValue,
+    bad: this.props.initialValue,
+  };
+
+  leaveFeedback(propName) {
+    this.setState(prevState => ({ [propName]: prevState[propName] + 1 }));
+  }
+
+  countTotalFeedback() {
+    const { good, bad, neutral } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    if (!good) return 0;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100);
+  }
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <>
+        <StyledMainTitle>Cafe Expresso</StyledMainTitle>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.leaveFeedback.bind(this)}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          <Statistics
+            {...this.state}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </>
+    );
+  }
 }
-export default App;
